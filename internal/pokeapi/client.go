3170f068bdf96b92f0bfacfc3ea44a2eed90b162
illgenl <lucaillgen04@gmail.com>
2026-07-09 15:21:10 +0200

    Created cache subpackage

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,10 +3,13 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/illgenl/pokedexcli/internal/pokecache"
 )
 
 type Client struct {
 	httpClient http.Client
+	cache      pokecache.Cache
 }
 
 func NewClient(timeout time.Duration) Client {
---
+++ internal/pokecache/go.mod
@@ -0,0 +1,3 @@
+module github.com/illgenl/pokedexcli/internal/pokecache
+
+go 1.26.4
